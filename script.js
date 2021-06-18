9050ab3d87441f0da1fc0cc5451480af6481af6a
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2021-06-18 13:06:04 +0200

    commit on 'master'

--- script.js
+++ script.js
@@ -113,6 +113,7 @@ setTimeout(() => {
 
 // Promises And The Fetch API
 // Consuming Promises
+// Chaining Promises
 
 // const request = fetch('https://restcountries.eu/rest/v2/name/south africa');
 // console.log(request);
@@ -128,10 +129,23 @@ setTimeout(() => {
 // };
 
 const getCountryData = function (country) {
+  // Country 1
   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
     .then((response) => response.json())
-    .then((data) => renderCountry(data[0]));
-};
+    .then((data) => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+      return 23;
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
+}; 
 
 getCountryData('south africa');
+
 
