d2caf59f128211c64fd967e162b7228d39fe4e7d
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2021-06-26 14:08:50 +0200

    commit on 'master'

--- script.js
+++ script.js
@@ -508,8 +508,9 @@ console.log('FIRST');
 // } catch (err) {
 //   alert(err.message);
 // }
-
 */
+
+/*
 ///////////////////////////////////////
 // Returning Values from Async Functions
 const getPosition = function () {
@@ -565,6 +566,83 @@ console.log('1: Will get location');
   }
   console.log('3: Finished getting location');
 })();
+*/
+
+/*
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.eu/rest/v2/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.eu/rest/v2/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.eu/rest/v2/name/${c3}`);
+    // console.log([data1.capital, data2.capital, data3.capital]);
+
+  const data = await Promise.all([
+      getJSON(`https://restcountries.eu/rest/v2/name/${c1}`),
+    getJSON(`https://restcountries.eu/rest/v2/name/${c2}`),
+    getJSON(`https://restcountries.eu/rest/v2/name/${c3}`),
+  ]);
+console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    console.error(err);
+  }
+};
+get3Countries('south africa', 'china', 'usa');
+*/
+
+// Promise.race
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.eu/rest/v2/name/italy`),
+    getJSON(`https://restcountries.eu/rest/v2/name/egypt`),
+    getJSON(`https://restcountries.eu/rest/v2/name/mexico`),
+  ]);
+  console.log(res[0]);
+})();
+
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error('Request took too long'))
+    }, sec * 1000);
+  });
+};
+
+Promise.race([
+  getJSON(`https://restcountries.eu/rest/v2/name/tanzania`),
+  timeout(5),
+])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err));
+
+// Promise.allSettled
+Promise.allSettled([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+])
+  .then(res => console.log(res));
+
+Promise.all([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+
+  // Promise.any [ES2021]
+  Promise.any([
+    Promise.resolve('Success'),
+    Promise.reject('ERROR'),
+    Promise.resolve('Another success'),
+  ])
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+
+
+
+
+
 
 
 
